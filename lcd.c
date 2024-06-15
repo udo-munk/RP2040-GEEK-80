f7186f2c161f7c1ca6fcd6ecb63cb71c5cf6ca77
Udo Munk <udo.munk@freenet.de>
2024-06-15 18:55:29 +0200

    properly programmed LCD

--- lcd.c
+++ lcd.c
@@ -8,53 +8,36 @@
 
 void lcd_init(void)
 {
+	/* initialize LCD device */
 	if (DEV_Module_Init() != 0)
 	{
 		return;
 	}
 
-	DEV_SET_PWM(0);
+	/* set orientation */
 	LCD_1IN14_V2_Init(HORIZONTAL);
 	LCD_1IN14_V2_Clear(BLACK);
-	DEV_SET_PWM(100);
 
-    UWORD *BlackImage;
-    UDOUBLE Imagesize = LCD_1IN14_V2_HEIGHT * LCD_1IN14_V2_WIDTH * 2;
-    if ((BlackImage = (UWORD *)malloc(Imagesize)) == NULL)
-    {
-        printf("Failed to apply for black memory...\r\n");
-        exit(0);
-    }
+	/* allocate memory for an image */
+	UWORD *BlackImage;
+	UDOUBLE Imagesize = LCD_1IN14_V2_HEIGHT * LCD_1IN14_V2_WIDTH * 2;
+	if ((BlackImage = (UWORD *)malloc(Imagesize)) == NULL)
+	{
+		printf("Failed to allocate memory\n");
+		exit(0);
+	}
 
-    // /*1.Create a new image cache named IMAGE_RGB and fill it with white*/
-    Paint_NewImage((UBYTE *)BlackImage, LCD_1IN14_V2.WIDTH, LCD_1IN14_V2.HEIGHT, 0, WHITE);
-
+	/* use this image memory */
+	Paint_NewImage((UBYTE *)BlackImage, LCD_1IN14_V2.WIDTH,
+			LCD_1IN14_V2.HEIGHT, 0, WHITE);
 	Paint_SetScale(65);
 	Paint_SetRotate(ROTATE_0);
 
-    Paint_Clear(WHITE);
-    Paint_DrawPoint(2, 1, BLACK, DOT_PIXEL_1X1, DOT_FILL_RIGHTUP); // 240 240
-    Paint_DrawPoint(2, 6, BLACK, DOT_PIXEL_2X2, DOT_FILL_RIGHTUP);
-    Paint_DrawPoint(2, 11, BLACK, DOT_PIXEL_3X3, DOT_FILL_RIGHTUP);
-    Paint_DrawPoint(2, 16, BLACK, DOT_PIXEL_4X4, DOT_FILL_RIGHTUP);
-    Paint_DrawPoint(2, 21, BLACK, DOT_PIXEL_5X5, DOT_FILL_RIGHTUP);
-    Paint_DrawLine(10, 5, 40, 35, MAGENTA, DOT_PIXEL_2X2, LINE_STYLE_SOLID);
-    Paint_DrawLine(10, 35, 40, 5, MAGENTA, DOT_PIXEL_2X2, LINE_STYLE_SOLID);
+	/* draw our stuff */
+	Paint_Clear(BLUE);
+	Paint_DrawString_EN(25, 40, "# Z80pack #", &Font24, BLACK, WHITE);
+	Paint_DrawString_EN(25, 64, "RP2040-GEEK", &Font24, BLACK, WHITE);
 
-    Paint_DrawLine(80, 20, 110, 20, CYAN, DOT_PIXEL_1X1, LINE_STYLE_DOTTED);
-    Paint_DrawLine(95, 5, 95, 35, CYAN, DOT_PIXEL_1X1, LINE_STYLE_DOTTED);
-
-    Paint_DrawRectangle(10, 5, 40, 35, RED, DOT_PIXEL_2X2, DRAW_FILL_EMPTY);
-    Paint_DrawRectangle(45, 5, 75, 35, BLUE, DOT_PIXEL_2X2, DRAW_FILL_FULL);
-
-    Paint_DrawCircle(95, 20, 15, GREEN, DOT_PIXEL_1X1, DRAW_FILL_EMPTY);
-    Paint_DrawCircle(130, 20, 15, GREEN, DOT_PIXEL_1X1, DRAW_FILL_FULL);
-
-    Paint_DrawNum(50, 40, 9.87654321, &Font20, 3, WHITE, BLACK);
-    Paint_DrawString_EN(1, 40, "ABC", &Font20, 0x000f, 0xfff0);
-    Paint_DrawString_CN(1, 60, "»¶Ó­Ê¹ÓÃ", &Font24CN, WHITE, BLUE);
-    Paint_DrawString_EN(1, 100, "Pico-GEEK", &Font16, RED, WHITE);
-
-        // /*3.Refresh the picture in RAM to LCD*/
-    LCD_1IN14_V2_Display(BlackImage);
+	/* refresh the picture in memory to LCD */
+	LCD_1IN14_V2_Display(BlackImage);
 }
