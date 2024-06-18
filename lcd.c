4d5ee4e1dd0c4f3442b9024b13d6d88be405b749
Udo Munk <udo.munk@freenet.de>
2024-06-18 11:29:00 +0200

    use static image buffer

--- lcd.c
+++ lcd.c
@@ -7,29 +7,18 @@
 #include "lcd.h"
 
 /* memory image for drawing */
-static UWORD *Image;
-static UDOUBLE Imagesize = LCD_1IN14_V2_HEIGHT * LCD_1IN14_V2_WIDTH * 2;
+static UWORD img[LCD_1IN14_V2_HEIGHT * LCD_1IN14_V2_WIDTH];
 
 int lcd_init(void)
 {
 	/* initialize LCD device */
-	if (DEV_Module_Init() != 0)
-	{
-		return 1;
-	}
+	DEV_Module_Init();
 
 	/* set orientation, x = 240 width and y = 135 height */
 	LCD_1IN14_V2_Init(HORIZONTAL);
-	LCD_1IN14_V2_Clear(BLACK);
-
-	/* allocate memory for an image */
-	if ((Image = (UWORD *) malloc(Imagesize)) == NULL)
-	{
-		return 2;
-	}
 
 	/* use this image memory */
-	Paint_NewImage((UBYTE *)Image, LCD_1IN14_V2.WIDTH,
+	Paint_NewImage((UBYTE *) &img[0], LCD_1IN14_V2.WIDTH,
 			LCD_1IN14_V2.HEIGHT, 0, WHITE);
 	/* with this scale */
 	Paint_SetScale(65);
@@ -42,12 +31,11 @@ int lcd_init(void)
 void lcd_refresh(void)
 {
 	/* refresh the picture in memory to LCD */
-	LCD_1IN14_V2_Display(Image);
+	LCD_1IN14_V2_Display(&img[0]);
 }
 
 void lcd_banner(void)
 {
-	Paint_Clear(BLUE);
 	Paint_DrawString_EN(25, 40, "# Z80pack #", &Font24, BLACK, WHITE);
 	Paint_DrawString_EN(25, 64, "RP2040-GEEK", &Font24, BLACK, WHITE);
 
@@ -56,7 +44,6 @@ void lcd_banner(void)
 
 void lcd_info(void)
 {
-	Paint_Clear(BLACK);
 	Paint_DrawString_EN(25, 40, "Waiting for", &Font24, RED, BLACK);
 	Paint_DrawString_EN(25, 64, "  terminal ", &Font24, RED, BLACK);
 
