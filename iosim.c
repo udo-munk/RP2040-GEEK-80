a9a189071e88e11d0fefa585ee3d3f10af4850d2
Udo Munk <udo.munk@freenet.de>
2024-06-24 09:55:23 +0200

    in system reset also reset LCD display to default

--- iosim.c
+++ iosim.c
@@ -27,6 +27,7 @@
 #include "sim.h"
 #include "simglb.h"
 #include "dazzler.h"
+#include "lcd.h"
 
 /*
  *	Forward declarations of the I/O functions
@@ -208,6 +209,7 @@ static void hwctl_out(BYTE data)
 	if (data & 64) {
 		reset_cpu();
 		PC = 0xff00;	/* power on jump */
+		lcd_default_draw_func();
 		return;
 	}
 
