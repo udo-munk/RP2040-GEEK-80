07817b66bf46eec41f66e943b508fd27ca52bd8b
Thomas Eberhardt <sneakywumpus@gmail.com>
2024-07-02 14:08:10 +0200

    disable ICE by default

--- sim.h
+++ sim.h
@@ -18,7 +18,7 @@
 /*#define FAST_BLOCK*/	/* much faster but not accurate Z80 block instr. */
 #define BAREMETAL	/* set up the simulator core for bare metal use */
 
-#define WANT_ICE	/* attach ICE to headless machine */
+/*#define WANT_ICE*/	/* attach ICE to headless machine */
 #ifdef WANT_ICE
 #define WANT_TIM	/* count t-states */
 #define HISIZE	100	/* number of entries in history */
