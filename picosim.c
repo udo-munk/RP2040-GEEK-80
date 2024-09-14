258fa939f117acebd56f16156a868e9e5dd1fda0
Udo Munk <udo.munk@freenet.de>
2024-09-14 08:20:42 +0200

    fix UART handling and enable, system can be used with a terminal connected to the UART now

--- picosim.c
+++ picosim.c
@@ -149,17 +149,20 @@ int main(void)
 
 	/* when using USB UART wait until it is connected */
 	/* but also get out if there is input at default UART */
+#if LIB_PICO_STDIO_UART
+	uart_inst_t *my_uart = uart_default;
+	/* destroy random input from UART after activation */
+	if (uart_is_readable(my_uart))
+		getchar();
+#endif
 #if LIB_PICO_STDIO_USB || (LIB_STDIO_MSC_USB && !STDIO_MSC_USB_DISABLE_STDIO)
 	lcd_custom_disp(lcd_draw_wait_term);
 	while (!tud_cdc_connected()) {
 #if LIB_PICO_STDIO_UART
-		// doesn't work, UART sometimes is readable even
-		// if nothing is connected
-		//uart_inst_t *my_uart = uart_default;
-		//if (uart_is_readable(my_uart)) {
-		//	getchar();
-		//	break;
-		//}
+		if (uart_is_readable(my_uart)) {
+			getchar();
+			break;
+		}
 #endif
 		sleep_ms(100);
 	}
